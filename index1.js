8b8dc8afd39373da5b7ec0fd61b4e637a7c7c43c
Aman Singh <amankumarkvm4@gmail.com>
2025-10-28 14:16:45 +0530

    Add initial Express app with MongoDB connection and EJS view rendering

--- index1.js
+++ index1.js
@@ -4,20 +4,20 @@ import { MongoClient } from 'mongodb'
 const dbName = "school" //database name
 const url = "mongodb://localhost:27017"
 
-
 //this intializes a client object to interact with mongodb server
 const client = new MongoClient(url) //connection to mongodb server
 
-async function dbConnection(){
+const app = express() //initializing express app
+app.set('view engine', 'ejs') //setting ejs as template engine
+
+
+app.get('/', async (req, resp) => {
     await client.connect() // establishing connection to mongodb server
     const db = client.db(dbName) //selecting database
     const collection = db.collection('students') //selecting collection
 
-    const result = await collection.find().toArray() //fetching all documents from collection
-    console.log(result)
-}
-dbConnection() //invoking the function to connect to db and fetch data
-
-const app = express() //initializing express app
-
-app.listen(3200) //
+    const students = await collection.find().toArray() //fetching all documents from collection
+    console.log(students)
+    resp.render('students',{students})
+})
+app.listen(3400) //server listening on port 3200
