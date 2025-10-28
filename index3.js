3055eb723c490a952a7a22dcf08e54edd4d8a1cd
Aman Singh <amankumarkvm4@gmail.com>
2025-10-28 21:41:48 +0530

    Initialize Express app with MongoDB connection and EJS view rendering

---
+++ index3.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import { MongoClient } from 'mongodb'
+
+
+const dbName = "school" //database name
+const url = "mongodb://localhost:27017"
+
+//this intializes a client object to interact with mongodb server
+const client = new MongoClient(url) //connection to mongodb server
+ 
+
+const app = express() //initializing express app
+
+app.set('view engine', 'ejs')
+client.connect().then((connection) => {
+    const db = connection.db(dbName)
+
+    app.get("/api", async(req, resp) => {
+        const collection = db.collection("students") //selecting collection
+        const students = await collection.find().toArray()
+        resp.send(students)
+    })
+    app.get("/ui", async(req, resp) => {
+        const collection = db.collection("students") //selecting collection
+        const students = await collection.find().toArray()
+        resp.render('students',{students})
+    })
+}) // establishing connection to mongodb server
+
+// app.set('view engine', 'ejs') //setting ejs as template engine
+
+
+// app.get('/', async (req, resp) => {
+//     await client.connect() // establishing connection to mongodb server
+//     const db = client.db(dbName) //selecting database
+//     const collection = db.collection('students') //selecting collection
+
+//     const students = await collection.find().toArray() //fetching all documents from collection
+//     console.log(students)
+//     resp.render('students',{students})
+// })
+app.listen(3400) //server listening on port 3200
